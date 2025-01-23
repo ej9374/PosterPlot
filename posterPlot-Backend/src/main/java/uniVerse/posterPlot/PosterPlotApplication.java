a6ccf209054fe5ea0258de2698c2e69aaf64963d
ej9374 <ej9374@naver.com>
2025-01-23 17:11:14 +0900

    backend 설정

---
+++ posterPlot-Backend/src/main/java/uniVerse/posterPlot/PosterPlotApplication.java
@@ -0,0 +1,13 @@
+package uniVerse.posterPlot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterPlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosterPlotApplication.class, args);
+	}
+
+}
---
+++ posterPlot-Backend/src/test/java/uniVerse/posterPlot/PosterPlotApplicationTests.java
@@ -0,0 +1,13 @@
+package uniVerse.posterPlot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterPlotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
