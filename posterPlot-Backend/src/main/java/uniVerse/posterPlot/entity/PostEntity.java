56904a6dde80006ac4305440479d515a2af7334e
ej9374 <ej9374@naver.com>
2025-01-25 17:16:27 +0900

    feat: Entity 생성

---
+++ posterPlot-Backend/src/main/java/uniVerse/posterPlot/entity/PostEntity.java
@@ -0,0 +1,50 @@
+package uniVerse.posterPlot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "post")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Integer postId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private UserEntity user;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "views")
+    private Integer views;
+
+    @Column(name = "total_likes")
+    private Integer totalLikes;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genre")
+    private Genre genre;
+
+    public PostEntity() {}
+
+    public PostEntity(UserEntity user, String title, String content, Integer views, Genre genre) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.views = views;
+        this.genre = genre;
+    }
+
+
+
+}
